d0ea8cb889cd4793c2574678546c6b828ef9d2cd
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2023-09-05 12:51:00 +0700

    solved challange-125

---
+++ challange-125.js
@@ -0,0 +1,14 @@
+// problem
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise. 
+
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// solution
+var isPalindrome = function(s) {
+    const nonAlphanumericStr = s.replace(/[\s+:,.?!_@#$&=+'"|\-\[\]{;}()`%^]/g, "");
+    return nonAlphanumericStr.toLowerCase() == nonAlphanumericStr.split('').reverse().join('').toLowerCase()
+};
