e2b6fc88eaf82f381d142878ca28798972199d34
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2025-11-04 07:38:28 +0700

    solved challange-67: add binary

---
+++ challange-67.ts
@@ -0,0 +1,8 @@
+// Given two binary strings a and b, return their sum as a binary string.
+// Example:
+// Input: a = "11", b = "1"
+// Output: "100"
+
+function addBinary(a: string, b: string): string {
+    return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2)
+};
