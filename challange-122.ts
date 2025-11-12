199af8063a25828a3d1c2c2561a3b58ef4bc5255
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2025-11-12 20:31:05 +0700

    Create challange-122.ts

---
+++ challange-122.ts
@@ -0,0 +1,22 @@
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. 
+// However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+// Find and return the maximum profit you can achieve.
+
+// example
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+
+function maxProfit(prices: number[]): number {
+  let totalProfit = 0;
+  if (prices.length < 2) return totalProfit;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      totalProfit += prices[i] - prices[i - 1];
+    }
+  }
+  return totalProfit;
+};
