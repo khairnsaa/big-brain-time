d9703f7bebc38fef952cdc4bbe463d4043115e89
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2026-01-01 20:31:48 +0700

    solved challange 66
    Implement plusOne function to increment large integer.

---
+++ challange-66.ts
@@ -0,0 +1,26 @@
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. 
+// The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+//  Example 1:
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+function plusOne(digits: number[]): number[] {
+    let strNum: string = ""
+    const answer: number[] = []
+    for(let i=0; i<digits.length; i++){
+        strNum += digits[i].toString()
+    }
+    let strAddedNum = (BigInt(strNum) + 1n).toString()
+    
+
+    for(let i=0; i<strAddedNum.length; i++){
+        answer.push(Number(strAddedNum[i]))
+    }
+    
+    return answer
+};
