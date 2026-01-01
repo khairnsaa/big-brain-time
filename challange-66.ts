9812890246ccf5a44654e627758e242909af8215
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2026-01-01 20:34:57 +0700

    add new solution to challange 66

--- challange-66.ts
+++ challange-66.ts
@@ -24,3 +24,18 @@ function plusOne(digits: number[]): number[] {
     
     return answer
 };
+
+// solution 2
+function plusOne(digits: number[]): number[] {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    // If all digits were 9
+    digits.unshift(1);
+    return digits;
+}
