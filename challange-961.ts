963589df9af7b56d0851ace79b924f8c2277ceea
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2026-01-02 16:44:41 +0700

    topic: array, hash table
    Implement function to find the element repeated n times in an array.

---
+++ challange-961.ts
@@ -0,0 +1,23 @@
+// You are given an integer array nums with the following properties:
+// nums.length == 2 * n.
+// nums contains n + 1 unique elements.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+
+// Example 1:
+// Input: nums = [1,2,3,3]
+// Output: 3
+
+function repeatedNTimes(nums: number[]): number {
+    const mapNums: Map<number, number> = new Map()
+    const n = nums.length / 2
+
+    for(let i=0; i<nums.length; i++){
+        mapNums.set(nums[i], (mapNums.get(nums[i]) || 0 )+ 1)
+    }
+    for(const [key, count] of mapNums){
+        if(count === n){
+            return key
+        }
+    }
+};
