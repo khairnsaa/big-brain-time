98491f47d2d3fb7ea1b6f43407d70dbe780dc68b
Khairunnisa <53510222+khairnsaa@users.noreply.github.com>
2026-01-03 23:16:59 +0700

    topic: math
    Implement function to find smallest even multiple.

---
+++ challange-2413.ts
@@ -0,0 +1,9 @@
+// Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+// Example 1:
+// Input: n = 5
+// Output: 10
+// Explanation: The smallest multiple of both 5 and 2 is 10.
+
+function smallestEvenMultiple(n: number): number {
+    return n%2 === 0 ? n : n*2
+};
